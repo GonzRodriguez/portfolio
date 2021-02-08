6a57b42971ff672fc3d46af92181719702a79eba
GONZdev <grgsalamanca@gmail.com>
2021-02-08 05:58:43 +0000

    My work finished

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const drawerList = document.querySelector(".drawer__list");
 const drawerMenu = document.querySelector(".drawer__menu");
 const servicesOnScreen = document.querySelector(".services__greenBlock");
 const myWorkItems = document.querySelectorAll(".myWork__item");
+const openDescriptionButtons = document.querySelectorAll(".myWork__item__description__open-button");
+const closeDescriptionButtons = document.querySelectorAll(".myWork__item__description__close-button");
 
 
 function hamburguerToggle(){
@@ -17,78 +19,110 @@ function hamburguerToggle(){
     drawerList.classList.toggle("open");
     drawerMenuBg.classList.toggle("open");
     drawerMenu.classList.toggle("open")
-    handleFloatMenuClass();
-
+    hamburguer.classList.toggle("floatMenu")
+    if (hamburguer.classList.value.includes("on")) {
+        hamburguer.classList.remove("floatMenu")
+    }
+    window.scrollY == 0 && hamburguer.classList.remove("floatMenu") 
+    
+    // handleFloatMenuClass();
 }
 hamburguer.addEventListener("click", () => {
     hamburguerToggle()
 
 })
 
-function handleFloatMenuClass() {
-    if (hamburguer.classList.value.includes("on")) {
-        hamburguer.classList.remove("floatMenu")
-    }
+// function addCloseButton(item, button) {
+//     const crossStripe1 = document.createElement("div");
+//     const crossStripe2 = document.createElement("div");
+//     crossStripe1.style = " transform: rotate( 45deg )";
+//     crossStripe2.style = " transform: rotate( 135deg )";
+//     item.appendChild(crossStripe1)
+//     item.appendChild(crossStripe2)
 
-    if (window.scrollY > 0 ) {
-        if ( !hamburguer.classList.value.includes("on")) {
-        hamburguer.classList.add("floatMenu")
-        }
-    }
-    if (window.scrollY == 0) {
-        hamburguer.classList.remove("floatMenu")
-    }
-    }
+//     crossStripe1.addEventListener("click", () => {
+//         button.classList.toggle("hidden");
+//         item.classList.toggle("open");
+//     })
+//     crossStripe2.addEventListener("click", () => {
+//         button.classList.toggle("hidden");
+//         item.classList.toggle("open");
+//     })
+
+// }
+
+openDescriptionButtons.forEach(button => {
+    const item = button.parentElement.parentElement
+    const crossStripe1 = document.createElement("div");
+    const crossStripe2 = document.createElement("div");
+    crossStripe1.style = " transform: rotate( 45deg )";
+    crossStripe2.style = " transform: rotate( 135deg )";
+ 
+
+    button.addEventListener("click", () => {
+        button.classList.toggle("hidden");
+        item.classList.toggle("open");
+        item.querySelector("ul").classList.toggle("hidden");
+
+        crossStripe1.classList.add("myWork__item__close-button");
+        crossStripe2.classList.add("myWork__item__close-button");
+
+        item.appendChild(crossStripe1)
+        item.appendChild(crossStripe2)
 
 
-window.addEventListener("scroll", () => {
-    handleFloatMenuClass();
-    handleGreenBoxAnimation()
 
-})
+        setTimeout(() => {
+            item.scrollIntoView();
+            
+        }, 100);
 
-function handleGreenBoxAnimation() {
-
-    
-    if (window.pageYOffset >= 550) {
-        servicesOnScreen.classList.add("onScreen");
-    }
-    if (window.pageYOffset <= 550) {
-        servicesOnScreen.classList.remove("onScreen");
-    }
-
-}
-myWorkItems.forEach(item => {
-    item.addEventListener("mouseenter", () => {
-        item.querySelector(".myWork__item__description").classList.add("open")
-        item.querySelector(".divider").classList.remove("hidden")
     })
-    item.addEventListener("mouseleave", () => {
-        item.querySelector(".myWork__item__description").classList.remove("open")
-        item.querySelector(".divider").classList.add("hidden")
+    crossStripe1.addEventListener("click", () => {
+        button.classList.toggle("hidden");
+        item.classList.toggle("open");
+        item.querySelector("ul").classList.toggle("hidden");
+        crossStripe1.classList.remove("myWork__item__close-button");
+        crossStripe2.classList.remove("myWork__item__close-button");
+
+    })
+    crossStripe2.addEventListener("click", () => {
+        button.classList.toggle("hidden");
+        item.classList.toggle("open");
+        item.querySelector("ul").classList.toggle("hidden");
+        crossStripe1.classList.remove("myWork__item__close-button");
+        crossStripe2.classList.remove("myWork__item__close-button");
     })
 });
 
 
-
 function inViewport(element) {
 
-    const bb = element.getBoundingClientRect();
-    return !(bb.top > innerHeight || bb.bottom < 0);    
+    const el = element.getBoundingClientRect();
+    return !(el.top > innerHeight || el.bottom < 0);
+}
+function delayAddClass(itemIndex, item, newClass) {
+    const delay = (itemIndex + 1) * 50
+    setTimeout(() => {
+         item.classList.add(newClass)
+    }, delay);
 }
 
-document.addEventListener('scroll', event => {
 
+document.addEventListener('scroll', () => {
+
+    window.scrollY > 0 && !hamburguer.classList.value.includes("on")
+    ? hamburguer.classList.add("floatMenu") : hamburguer.classList.remove("floatMenu");
+
+    
     myWorkItems.forEach(item => {
-
-        if (inViewport(item)) {
-                item.classList.add("onViewport")
-
-        } else {
-
-            item.classList.remove("onViewport")
         
-    }
+        // console.log(item);
+        if (inViewport(item)) {
+            delayAddClass(Array.from(myWorkItems).indexOf(item), item, "onViewport")
+
+        }
     });
 
 })
+
