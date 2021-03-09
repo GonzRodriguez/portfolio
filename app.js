bae47e996db9d767038769a7ec3dc0b7784c5bc0
GONZdev <grgsalamanca@gmail.com>
2021-03-09 19:20:50 +0000

    my work section finished, fixing little bugs and adding the final content

--- app.js
+++ app.js
@@ -13,6 +13,26 @@ const myWorkItems = document.querySelectorAll(".myWork__item");
 const myWorkGroup = document.querySelector(".myWork__group");
 const openDescriptionButtons = document.querySelectorAll(".myWork__item__description__open-button");
 const closeDescriptionButtons = document.querySelectorAll(".myWork__item__description__close-button");
+const copyright = document.querySelector(".copyright");
+
+
+function closeButton(){
+
+    const cross = document.createElement("div");
+    const crossStripe1 = document.createElement("div");
+    const crossStripe2 = document.createElement("div");
+
+    crossStripe1.style = " transform: rotate( 45deg )";
+    crossStripe2.style = " transform: rotate( 135deg )";
+
+    cross.classList.add("close__button__Element");
+    crossStripe1.classList.add("close__button__stripe");
+    crossStripe2.classList.add("close__button__stripe");
+
+    cross.appendChild(crossStripe1)
+    cross.appendChild(crossStripe2)
+    return cross
+}
 
 
 function hamburguerToggle(){
@@ -29,46 +49,37 @@ function hamburguerToggle(){
 }
 hamburguer.addEventListener("click", () => {
     hamburguerToggle()
-
 })
-
+// handle each "show more" button in my work section 
 openDescriptionButtons.forEach(button => {
-    const item = button.parentElement.parentElement
-    const crossStripe1 = document.createElement("div");
-    const crossStripe2 = document.createElement("div");
-    crossStripe1.style = " transform: rotate( 45deg )";
-    crossStripe2.style = " transform: rotate( 135deg )";
- 
-
+    const item = button.parentElement.parentElement.parentElement
+    const workItem__cover = item.querySelector(".myWork__item__cover")
+    const cross = closeButton()
+    
     button.addEventListener("click", async () => {
+        if (item.id === "right-column"){
+            item.classList.add("right-column");
+        }
         button.classList.toggle("hidden");
+        cross.classList.remove("hidden");
         item.classList.toggle("open");
+        workItem__cover.classList.toggle("open");
         item.querySelector("ul").classList.toggle("hidden");
-        crossStripe1.classList.add("myWork__item__close-button");
-        crossStripe2.classList.add("myWork__item__close-button");
-
-        item.appendChild(crossStripe1)
-        item.appendChild(crossStripe2)
-
-        // myWorkGroup.scrollIntoView();
+        item.appendChild(cross)
     })
 
-    crossStripe1.addEventListener("click", () => {
+    cross.addEventListener("click", () => {
         button.classList.toggle("hidden");
         item.classList.toggle("open");
+        item.classList.remove("right-column");
+        workItem__cover.classList.toggle("open");
         item.querySelector("ul").classList.toggle("hidden");
-        crossStripe1.classList.remove("myWork__item__close-button");
-        crossStripe2.classList.remove("myWork__item__close-button");
+        cross.classList.add("hidden");
+        cross.classList.remove(".close__button__Element");
 
     })
 
-    crossStripe2.addEventListener("click", () => {
-        button.classList.toggle("hidden");
-        item.classList.toggle("open");
-        item.querySelector("ul").classList.toggle("hidden");
-        crossStripe1.classList.remove("myWork__item__close-button");
-        crossStripe2.classList.remove("myWork__item__close-button");
-    })
+
 });
 
 
@@ -86,19 +97,58 @@ function delayAddClass(itemIndex, item, newClass) {
 
 
 document.addEventListener('scroll', () => {
-
+    
     window.scrollY > 0 && !hamburguer.classList.value.includes("on")
     ? hamburguer.classList.add("floatMenu") : hamburguer.classList.remove("floatMenu");
-
+    
     
     myWorkItems.forEach(item => {
         
         // console.log(item);
         if (inViewport(item)) {
             delayAddClass(Array.from(myWorkItems).indexOf(item), item, "onViewport")
-
+            
         }
     });
+    
+})
+
+
+// handling zooming images on "myWork" section 
+myWorkItems.forEach(item => {
+
+    const zoomedImg = document.createElement("div");
+    const cross = closeButton()
+    let clnImg = ""
+    // get all the pictures and add "onclick" functionality
+    const myWorkListImg = item.querySelectorAll(".myWork__item__more-info__item")
+
+    myWorkListImg.forEach(image => {
+        image.onclick = e => {
+            clnImg = e.target.cloneNode(true);
+            if(clnImg.tagName === "IMG"){
+                zoomedImg.appendChild(clnImg)
+                zoomedImg.appendChild(cross)
+                zoomedImg.classList.add("zoomedImg")
+                // add the div before the body
+                document.body.prepend(zoomedImg)
+                console.log(zoomedImg);
+            }
+        }
+
+        cross.addEventListener("click", () => {
+            zoomedImg.removeChild(zoomedImg.firstChild)
+            zoomedImg.classList.remove("zoomedImg")
+            })
+    })
 
 })
 
+function addYearToCopy() {
+    const year = new Date().getFullYear();
+    const span = document.createElement("span")
+    span.innerText = year
+    copyright.appendChild(span)
+}
+
+addYearToCopy()
